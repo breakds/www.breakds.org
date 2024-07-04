f5e682055140303927ee12aa5f4bc7331c7006bb
Break Yang <breakds@gmail.com>
2024-07-04 14:22:40 -0700

    Upgrade to 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "www.breakds.org the website";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/21.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
 
     utils.url = "github:numtide/flake-utils";
     utils.inputs.nixpkgs.follows = "nixpkgs";
